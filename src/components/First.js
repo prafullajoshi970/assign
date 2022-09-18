2cf400811e5d196fcaa72307cde8355439475594
prafullajoshi970 <97543959+prafullajoshi970@users.noreply.github.com>
2022-09-19 01:20:35 +0530

    Add files via upload

---
+++ src/components/First.js
@@ -0,0 +1,99 @@
+const First = () => {
+    return (
+      
+      
+<div className="main-box">
+<div className="left">
+<div className="side_main">
+⏺️
+
+<h4>Prafulla Joshi</h4>
+<p className="side-para">🏠 Home</p>
+<p className="side-para">💸 Deposits</p>
+<p className="side-para">🤹‍♀️ Offers</p>
+<p className="side-para">💰 Payments</p>
+<p className="side-para">⚒️ Settings</p>
+<div className="toggle">
+</div></div></div><div className="right">
+<div className="head-section"><div className="right-cont">
+⏺️
+😊
+</div>
+</div><div className="tab-title"><div className="tt-left"><h2 className="tt-head">Active Deposits</h2>
+<button className="tt-btn">2</button></div>
+<div className="tt-right"><button className="tt-r-btn">+</button>
+</div>
+</div><div className="table-cont">
+<table>
+<thead>
+<tr>
+<th>Property</th>
+<th></th>
+<th></th>
+<th>Move In Date</th>
+<th>Rent</th>
+<th>Deposit</th>
+<th>Status</th>
+<th></th></tr>
+</thead><tbody>
+<tr><td>
+⏺️</td>
+<td><h3>Prafulla Joshi</h3>
+</td><td></td><td>18-09-2022</td>
+<td>$2000</td><td>$10000</td>
+<td>Bank Processing</td><td>
+<button className="tb-status"></button>
+</td></tr>
+<tr><td>
+⏺️</td>
+<td><h3>Prafulla Joshi</h3></td>
+<td></td>
+<td>18-09-2022</td><td>$2000</td>
+<td>$10000</td>
+<td>Bank Processed</td><td>
+    <button className="tb-status"></button>
+</td></tr>
+</tbody></table>
+<div className="tb-outer">
+    <button className="tb-outer-btn">All Active Deposits</button>
+</div></div>
+<div className="tab-title"><div className="tt-left">
+    <h2 className="tt-head">Closed Deposits</h2>
+<button className="tt-btn">8</button>
+</div></div>
+<div className="table-cont">
+    <table><thead><tr><th>Property</th><th>
+
+</th>
+<th></th>
+<th>Move In Date</th>
+<th>Rent</th>
+<th>Deposit</th>
+<th>Status</th><th>
+    </th></tr></thead><tbody><tr><td>⏺️
+    </td><td><h3>Prafulla Joshi</h3></td>
+<td></td>
+<td>18-09-2022</td>
+<td>$2000</td>
+<td>$10000</td>
+<td>Bank Processing</td>
+<td><button className="tb-status" ></button>
+</td></tr>
+<tr><td>⏺️</td><td>
+    <h3>Prafulla Joshi</h3></td>
+<td></td>
+<td>18-09-2022</td>
+<td>$2000</td>
+<td>$10000</td>
+<td>Bank Processed</td><td>
+    <button className="tb-status"></button>
+</td>
+</tr>
+</tbody>
+</table>
+<div className="tb-outer">
+    <button className="tb-outer-btn">All Closed Deposits</button></div></div></div>
+</div>
+    )}
+  
+  export default First;
